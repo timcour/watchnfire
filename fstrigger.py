f4e7788fca3619f6314596131e3efa6f1e5839de
Tim Courrejou <timcour@gmail.com>
2013-08-11 21:50:00 -0700

    run firing queue in background

--- fstrigger.py
+++ fstrigger.py
@@ -9,8 +9,9 @@ import signal
 import sys
 import time
 
-from subprocess import Popen, PIPE
 from fsevents import Observer, Stream
+from subprocess import Popen, PIPE
+from threading import Timer
 
 def is_flag(testval):
     if not isinstance(testval, int):
@@ -33,21 +34,35 @@ def get_event_flags(mask):
     return flags.keys()
 
 class EventTriggerManager(object):
-    def __init__(self, triggers):
+    def __init__(self, triggers, queue_execution_wait=0.2):
         self.triggers = triggers
         self.observer = Observer()
-        self.firing_queue = []
 
+        self.firing_queue = []
+        self.firing_wait = queue_execution_wait
+        self.firing_queue_thread = Timer(self.firing_wait, self.execute_firing_queue)
         self._is_executing_firing_queue = False
 
     def queue_firing_trigger(self, trigger):
         if trigger not in self.firing_queue:
             print "adding %s to firing_queue" % trigger
             self.firing_queue.insert(0, trigger)
-        self.execute_firing_queue()
+
+        if (not self._is_executing_firing_queue
+            and self.firing_queue_thread.is_alive()):
+            print "received another queue request, canceling timer"
+            self.firing_queue_thread.cancel()
+            self.firing_queue_thread.join()
+
+        if not self.firing_queue_thread.is_alive():
+            self.firing_queue_thread = Timer(self.firing_wait, self.execute_firing_queue)
+            print "starting new timer"
+            self.firing_queue_thread.start()
 
     def execute_firing_queue(self):
+        print "executing firing queue"
         if self._is_executing_firing_queue:
+            "execution in progress"
             return
         self._is_executing_firing_queue = True
         while len(self.firing_queue):
@@ -64,6 +79,9 @@ class EventTriggerManager(object):
     def stop(self):
         for pt in self.triggers:
             self.observer.unschedule(pt.stream)
+            # kill any process being run by the trigger now that it
+            # cannot be rescheduled
+            pt.killfire()
         self.observer.stop()
         self.observer.join()
 
@@ -71,22 +89,27 @@ class PathTrigger(object):
     def __init__(self, path, extensions, command, ignore_file_pattern="^\.#.*$"):
         self.path = path
         self.regexp = re.compile(".*\.(%s)$" % '|'.join(extensions.split(',')))
+        self.file_ignore_regexp = re.compile(ignore_file_pattern)
         self.command = command
         self.stream = Stream(self.handle_event, self.path, file_events=True)
-
-        self.file_ignore_regexp = re.compile(ignore_file_pattern)
+        self.proc = None
 
         # should be set by a user of this object
         self.schedule_execution = lambda x: None
 
     def fire(self):
         print "---> firing: %s" % self.command
-        proc = Popen(self.command, shell=True, stdout=PIPE, stderr=PIPE)
-        print proc.stdout.read()
-        for stream in map(lambda x: getattr(proc, x), ['stdin', 'stdout', 'stderr']):
+        self.proc = Popen(self.command, shell=True, stdout=PIPE, stderr=PIPE)
+        print self.proc.stdout.read()
+        for stream in map(lambda x: getattr(self.proc, x), ['stdin', 'stdout', 'stderr']):
             if stream:
                 stream.close()
-        proc.wait()
+        self.proc.wait()
+        self.proc = None
+
+    def killfire(self):
+        if self.proc:
+            self.proc.kill()
 
     def handle_event(self, event):
         print event.name, event.cookie, get_event_flags(event.mask)
